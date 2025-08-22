0551a3f32295922d83c77c239bdf09a16f7556a3
Anurag <anurag.gandhi@emurgo.io>
2025-08-22 12:37:08 +0530

    add_workflow

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+base: '/eased.ca/'  // Add this line
+
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
